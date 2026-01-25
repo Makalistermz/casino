d49725e8a40410b9ba257e4a3af644e7038b6945
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-01-25 10:41:58 -0300

    ..

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function girar() {
+    let casanik = document.getElementsByClassName('casanik');
+    let um = document.getElementById('um');
+    let dois = document.getElementById('dois');
+    let tres = document.getElementById('tres');
+
+    
+}
