ee5ec0bdc31e108a5c785e0abc19b6ea7cd067c7
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-02-21 10:01:17 -0300

    .

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ function girar() {
     let um = document.getElementById('um');
     let dois = document.getElementById('dois');
     let tres = document.getElementById('tres');
-    let res = document.getElementById('res')
+    let res = document.getElementById('res');
+    let saldo = document.querySelector('#saldo');
 
     const casino = ['🍒', '🍋', '🔔', '⭐', '💎'];
+
 
     let n1 = Math.floor(Math.random() * casino.length);
     let n2 = Math.floor(Math.random() * casino.length);
@@ -16,6 +18,16 @@ function girar() {
     tres.innerHTML = casino[n3];
 
     if (n1 == n2 && n2 == n3) {
+        let saldoAtual = Number(saldo.textContent);
+
+        if (casino[n1] === casino[0]) saldoAtual += 10;
+        if (casino[n1] === casino[1]) saldoAtual += 15;
+        if (casino[n1] === casino[2]) saldoAtual += 25;
+        if (casino[n1] === casino[3]) saldoAtual += 40;
+        if (casino[n1] === casino[4]) saldoAtual += 100;
+
+        saldo.textContent = saldoAtual
+
         res.innerHTML = 'Você ganhou!! 🎉🎉🎉'
     } else {
         res.innerHTML = 'Não ganhou!! Tente novamente 🤣🤣'
